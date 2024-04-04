a074b5ec5412dca5400c8fb9860e15ca8c6d9372
T Shivani <96899982+shivani-tripurari@users.noreply.github.com>
2024-04-04 21:16:14 +0530

    modify

--- fronten/src/components/Footer.jsx
+++ fronten/src/components/Footer.jsx
@@ -6,22 +6,22 @@ function Footer(){
             <div className='footer-section'>
                 <div className='sections'>
                     <div className='left-footer'>
-                        <h4><span>Lorem, ipsum dolor.</span></h4>
-                        <h4>Lorem, ipsum.</h4>
-                        <h4>Lorem, ipsum.</h4>
+                        <h4><span>Serve, Community, Grow.</span></h4>
+                        <h4>Indra nagar</h4>
+                        <h4>Bengaluru, India.</h4>
                     </div>
                     <div className='right-footer'>
-                        <h4><span>Lorem.</span></h4>
-                        <h4>Lorem, ipsum.</h4>
+                        <h4><span>Contact.</span></h4>
+                        <h4>+91633889.</h4>
                         <h4>Lorem, ipsum.</h4>
                     </div>
                     <div className='center-footer'>
-                        <h4><span>Lorem, ipsum dolor.</span></h4>
+                        <h4><span>Services.</span></h4>
                         <h4>Lorem, ipsum.</h4>
                         <h4>Lorem, ipsum.</h4>
                     </div>
                     <div className='right-footer'>
-                        <h4><span>Lorem, ipsum dolor.</span></h4>
+                        <h4><span>Contribute.</span></h4>
                         <h4>Lorem, ipsum dolor.</h4>
                         <h4>Lorem, ipsum.</h4>
                     </div>
